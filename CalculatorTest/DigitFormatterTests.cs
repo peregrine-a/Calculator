a6ab661733028ddaab86c6f664cd00d8a7e47c48
peregrine.a <fire.peregrine.a@gmail.com>
2024-02-11 16:41:03 +0900

    テストメソッドの追加
     1. 動作確認のため, 単純なテストメソッドを追加してみた.

---
+++ CalculatorTest/DigitFormatterTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Calculator;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class DigitFormatterTests
+    {
+        [TestMethod]
+        public void MyTest()
+        {
+            decimal value1 =  123;
+            decimal value2 =  456;
+
+            Assert.AreEqual(value1 + value2, 579);
+
+            decimal value3 = 1000;
+            decimal value4 =  234;
+
+            Assert.AreEqual(value3 + value4, 1234);
+        }
+
+        // [TestMethod]
+        // public void DigitFormatterTests()
+        // {
+        // }
+    }
+}
